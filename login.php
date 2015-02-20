3d9755c907cf3909f1485a131b68a09feeb92dd6
gurnit1 <gurnit@att.net>
2015-02-19 22:55:04 -0800

    login - need to fix redirect to dashboard

---
+++ login.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+	try {
+		$con = new PDO("mysql:host=localhost;dbname=mentorweb", "root", "root");
+		$con->setAttribute(PDO::ATTR_ERRMODE,
+						   PDO::ERRMODE_EXCEPTION);
+		if (isset($_POST['submitLogin'])) {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			checkIfUserExists($_POST['username'], $_POST['password']);
+		} 
+	} catch(PDOException $ex) {
+		echo "<p>Connection failed</p> $ex";
+	}
+/*
+function connect() {
+	return mysqli_connect("localhost", "root", "root", "mentorweb");
+}
+*/
+
+function checkIfUserExists($username, $password) {
+	global $con;
+	$sql = "
+			SELECT * 
+			FROM userData
+			WHERE username=:username AND password=:password
+			LIMIT 1";
+	$q = $con->prepare($sql);
+	$q->execute(array(':username' => $username, ':password' => $password));
+	$rows = $q->fetchAll(PDO::FETCH_ASSOC);
+	if (count($rows) == 0) {
+		echo "No matching row    $username 	$password";
+		return 0;
+	} else {
+		//foreach ($rows as $user)
+		$firstName = $rows[0]['firstName'];
+		$lastName = $rows[0]['lastName'];
+		if ($rows[0]['mentor'] == 1) {
+			$isMentor = "Yes";
+		} else {
+			$isMentor = "No";
+		}
+		if ($rows[0]['mentee'] == 1) {
+			$isMentee = "Yes";
+		} else {
+			$isMentee = "No";
+		}
+		if ($rows[0]['lookingForMatch'] == 1) {
+			$isLookingForMatch = "Yes";
+		} else {
+			$isLookingForMatch = "No";
+		}
+  	  	print <<<HERE
+<p>
+User exists!
+</p>
+<br/>
+<table border="1">
+	<tr>
+		<th>Username</th>
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th>Mentor</th>
+		<th>Mentee</th>
+		<th>Looking for Match</th>
+	</tr>
+	<tr>
+		<td>$username</td>
+		<td>$firstName</td>
+		<td>$lastName</td>
+		<td>$isMentor</td>
+		<td>$isMentee</td>
+		<td>$isLookingForMatch</td>
+	</tr>
+</table>		
+HERE;
+	    return 1;
+	}
+}
+?>
+</body>
+</html>
