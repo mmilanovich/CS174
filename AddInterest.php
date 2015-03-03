16764833240af062ebfc758a7319dd82eee5efa0
mmilanovich <mmilanovich@gmail.com>
2015-03-02 20:26:05 -0800

    Trying to be able to add an interest on profile page

---
+++ AddInterest.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	$username = $_SESSION['username'];
+	$interest = $_GET['interest'];
+	try {
+		$con = new PDO("mysql:host=localhost;dbname=mentorweb", "root", "root");
+		$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		try { 
+			$sql = "
+					INSERT INTO iterests (interest)
+					VALUES ('$interest')";
+			$q = $con->prepare($sql);
+			$q->execute();
+		} catch(PDOException $ex) {
+		}
+		$sql = "
+			INSERT INTO user_interests (user_id, interest_id)
+			SELECT username, id
+			FROM interests, userdata
+			WHERE interest='$interest' AND username='$username'
+			";
+		$q = $con->prepare($sql);
+		$q->execute();
+		header('Location: profilePage.html'); 
+	} catch(PDOException $ex) {
+		echo "<p>Connection failed</p> $ex";
+	}
+?>
